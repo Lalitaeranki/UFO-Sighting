727dcfd8f5ff8d688ab6c8a80c0cf667b526e852
Lalitaeranki <37825444+Lalitaeranki@users.noreply.github.com>
2018-11-04 22:54:30 -0800

    Add files via upload

---
+++ static/js/app.js
@@ -0,0 +1,59 @@
+// from data.js
+let tableData = data;
+let tbody = d3.select("tbody");
+
+
+function getTable(ufoDatas) {
+	ufoDatas.forEach(datas => {
+		var row = tbody.append("tr");
+		Object.values(datas).forEach(value =>{
+        var cell = row.append("td");
+        cell.text(value);
+	});
+	});	
+}
+    
+   let submit = d3.select("#filter-btn");
+   let reset = d3.select("#reset-btn");
+   reset.on("click",function(){
+    tbody.html('');
+    getTable(tableData);
+   });
+   submit.on("click", function() {
+   
+     // Prevent the page from refreshing
+     d3.event.preventDefault();
+   
+     // Select the input element and get the raw HTML node
+     let searchDate = d3.select("#datetime").property("value");
+     let searchCity = d3.select("#enterCity").property("value");
+     let searchCountry = d3.select("#enterCountry").property("value");
+     let searchState = d3.select("#enterState").property("value");
+     let searchShape = d3.select("#enterShape").property("value");
+   
+     let filteredData = data;
+
+	if (searchDate != ""){
+    	filteredData = filteredData.filter(filterdata => filterdata.datetime === searchDate);
+    }
+    if (searchCity !=""){
+    	filteredData = filteredData.filter(filterdata => filterdata.city.toLowerCase() === searchCity.toLowerCase());
+    }
+    if (searchState !=""){
+        filteredData = filteredData.filter(filterdata => filterdata.state.toLowerCase() === searchState.toLowerCase());
+        }
+    if (searchCountry !=""){
+        filteredData = filteredData.filter(filterdata => filterdata.country.toLowerCase() === searchCountry.toLowerCase());
+        }
+    if (searchShape !=""){
+        filteredData = filteredData.filter(filterdata => filterdata.shape.toLowerCase() === searchShape.toLowerCase());
+        }
+   
+        tbody.html('');
+        getTable(filteredData);
+    });
+    
+    
+    getTable(tableData);
+    
+    
